9c82038498789a7f9aa9de4cb77473a0c5cdde53
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-09-09 09:48:37 +0200

    Fixed the problem with the winningscreen in the second level

--- js/Player.js
+++ js/Player.js
@@ -11,14 +11,14 @@ class Player {
 			if (e.keyCode == 37) { this.xPos -= 10; };
 			if (e.keyCode == 38) { this.yPos -= 10; };
 			if (e.keyCode == 40) { this.yPos += 10; };
-			if (this.xPos <= 20) {this.xPos= 20};
-			if (this.yPos <= 20) {this.yPos= 20};
-			if (this.xPos+ this.radius > canvas.width) {this.xPos = canvas.width - this.radius};
-			if (this.yPos+ this.radius > canvas.height) {this.yPos = canvas.height - this.radius};
+			if (this.xPos <= 20) { this.xPos = 20 };
+			if (this.yPos <= 20) { this.yPos = 20 };
+			if (this.xPos + this.radius > canvas.width) { this.xPos = canvas.width - this.radius };
+			if (this.yPos + this.radius > canvas.height) { this.yPos = canvas.height - this.radius };
 		}
 	}
 	draw() {
-		this.context.save() 
+		this.context.save()
 		this.context.fillStyle = "red";
 
 		this.context.beginPath();
@@ -28,13 +28,13 @@ class Player {
 		this.context.restore()
 	}
 	win() {
+		if (this.xPos > 700 && this.yPos > 300 && this.level === 2) {
+			location.replace("./winningscreen.html");
+		}
 		if (this.xPos > 700 && this.yPos > 300 && this.level === 1) {
-		this.level = 2;
-		this.xPos = 20;
-		this.yPos = 20;
-		if (this.xPos > 700 && this.yPos > 300 && this.level === 2){
-		location.replace("./winingscreen.html");
+			this.level = 2;
+			this.xPos = 20;
+			this.yPos = 20;
 		}
 	}
-}
 }
