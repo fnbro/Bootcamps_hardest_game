f0ef3a8230646fe31610d1cf3ec0ce52242cf4ef
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-09-10 11:57:27 +0200

    the ball moves now smoothly

--- js/Player.js
+++ js/Player.js
@@ -2,21 +2,12 @@ class Player {
 	constructor(context, level) {
 		this.yPos = 40;
 		this.xPos = 20;
+		this.vx = 0;
+		this.vy = 0;
 		this.radius = 15;
-		this.speed = 3;
 		this.level = level;
 		this.context = context;
 		this.draw();
-		document.onkeydown = e => {
-			if (e.keyCode == 39) { this.xPos += 10; };
-			if (e.keyCode == 37) { this.xPos -= 10; };
-			if (e.keyCode == 38) { this.yPos -= 10; };
-			if (e.keyCode == 40) { this.yPos += 10; };
-			if (this.xPos <= 20) { this.xPos = 20 };
-			if (this.yPos <= 20) { this.yPos = 20 };
-			if (this.xPos + this.radius > canvas.width) { this.xPos = canvas.width - this.radius };
-			if (this.yPos + this.radius > canvas.height) { this.yPos = canvas.height - this.radius };
-		}
 	}
 	draw() {
 		this.context.save()
@@ -29,8 +20,30 @@ class Player {
 		this.context.restore()
 
 		this.context.font = "20px Arial";
-        this.context.fillText(`Level: ${this.level}`, 0, 20);
+		this.context.fillText(`Level: ${this.level}`, 0, 20);
 
+	}
+	update() {
+		this.xPos += 1 * this.vx;
+		this.yPos += 1 * this.vy;
+		if (this.xPos <= 20) { this.xPos = 20 };
+		if (this.yPos <= 20) { this.yPos = 20 };
+		if (this.xPos + this.radius > canvas.width) { this.xPos = canvas.width - this.radius };
+		if (this.yPos + this.radius > canvas.height) { this.yPos = canvas.height - this.radius };
+	}
+	move() {
+		document.onkeydown = e => {
+			if (e.keyCode === 37) { this.vx = -5; }
+			if (e.keyCode === 39) { this.vx = 5; }
+			if (e.keyCode === 38) { this.vy = -5; }
+			if (e.keyCode === 40) { this.vy = 5; }
+		}
+		document.onkeyup = e => {
+			if (e.keyCode === 37 && this.vx < 0) { this.vx = 0; }
+			if (e.keyCode === 39 && this.vx > 0) { this.vx = 0; }
+			if (e.keyCode === 38 && this.vy < 0) { this.vy = 0; }
+			if (e.keyCode === 40 && this.vy > 0) { this.vy = 0; }
+		}
 	}
 	win() {
 		if (this.xPos > 700 && this.yPos > 300 && this.level === 3) {
@@ -49,3 +62,16 @@ class Player {
 		}
 	}
 }
+// the old movement of the ball
+/*
+document.onkeydown = e => {
+	if (e.keyCode == 39) { this.xPos += 10; };
+	if (e.keyCode == 37) { this.xPos -= 10; };
+	if (e.keyCode == 38) { this.yPos -= 10; };
+	if (e.keyCode == 40) { this.yPos += 10; };
+	if (this.xPos <= 20) { this.xPos = 20 };
+	if (this.yPos <= 20) { this.yPos = 20 };
+	if (this.xPos + this.radius > canvas.width) { this.xPos = canvas.width - this.radius };
+	if (this.yPos + this.radius > canvas.height) { this.yPos = canvas.height - this.radius };
+}
+*/
--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ class HardestGame {
     this.obstacle3 = new Obstacle(context, 700, 220, this.deathCounter);
     this.obstacle4 = new Obstacle(context, 40, 260, this.deathCounter);
     this.obstacle5 = new Obstacle(context, 700, 300, this.deathCounter);
-    this.bouncingBall = new BouncingBall (context, 123, 100, this.deathCounter);
-    this.bouncingBall1 = new BouncingBall (context, 290, 200, this.deathCounter);
-    this.bouncingBall2 = new BouncingBall (context, 360, 300, this.deathCounter);
+    this.bouncingBall = new BouncingBall(context, 123, 100, this.deathCounter);
+    this.bouncingBall1 = new BouncingBall(context, 290, 200, this.deathCounter);
+    this.bouncingBall2 = new BouncingBall(context, 360, 300, this.deathCounter);
     this.cross = new Cross(context, 200, 200, 200, 0.03, this.deathCounter)
     this.cross1 = new Cross(context, 600, 200, 200, 0.03, this.deathCounter)
     this.win = new Win(context);
@@ -22,42 +22,49 @@ class HardestGame {
   }
   draw() {
     if (this.player.level === 1) {
-    this.context.clearRect(0, 0, innerWidth, innerHeight)
-    this.player.draw(); 
-    this.obstacle.draw();
-    this.obstacle1.draw();
-    this.obstacle2.draw();
-    this.obstacle3.draw();
-    this.obstacle4.draw();
-    this.obstacle5.draw();
-    this.deathCounter.draw();;
-    this.win.draw();
-    window.requestAnimationFrame(this.draw);
-    this.player.win();
-    this.obstacle.checkCollision(this.player);
-    this.obstacle1.checkCollision(this.player);
-    this.obstacle2.checkCollision(this.player);
-    this.obstacle3.checkCollision(this.player);
-    this.obstacle4.checkCollision(this.player);
-    this.obstacle5.checkCollision(this.player);
+      this.context.clearRect(0, 0, innerWidth, innerHeight)
+      this.player.draw();
+      this.player.update();
+      this.player.move();
+      this.obstacle.draw();
+      this.obstacle1.draw();
+      this.obstacle2.draw();
+      this.obstacle3.draw();
+      this.obstacle4.draw();
+      this.obstacle5.draw();
+      this.deathCounter.draw();;
+      this.win.draw();
+      window.requestAnimationFrame(this.draw);
+      this.player.win();
+      this.obstacle.checkCollision(this.player);
+      this.obstacle1.checkCollision(this.player);
+      this.obstacle2.checkCollision(this.player);
+      this.obstacle3.checkCollision(this.player);
+      this.obstacle4.checkCollision(this.player);
+      this.obstacle5.checkCollision(this.player);
     }
     if (this.player.level === 2) {
-    this.context.clearRect(0, 0, innerWidth, innerHeight)
-    this.deathCounter.draw();
-    this.win.draw();
-    window.requestAnimationFrame(this.draw);
-    this.player.win();
-    this.cross.draw();
-    this.cross.update();
-    this.cross1.draw();
-    this.cross1.update();
-    this.cross.checkCollision(this.player);
-    this.cross1.checkCollision(this.player);
-    this.player.draw();
+      this.context.clearRect(0, 0, innerWidth, innerHeight)
+      this.deathCounter.draw();
+      this.player.draw();
+      this.player.update();
+      this.player.move();
+      this.win.draw();
+      window.requestAnimationFrame(this.draw);
+      this.player.win();
+      this.cross.draw();
+      this.cross.update();
+      this.cross1.draw();
+      this.cross1.update();
+      this.cross.checkCollision(this.player);
+      this.cross1.checkCollision(this.player);
+      this.player.draw();
     }
     if (this.player.level === 3) {
       this.context.clearRect(0, 0, innerWidth, innerHeight)
       this.player.draw();
+      this.player.update();
+      this.player.move();
       setInterval(this.bouncingBall.draw(), 10);
       setInterval(this.bouncingBall1.draw(), 10);
       setInterval(this.bouncingBall2.draw(), 10);
@@ -68,11 +75,12 @@ class HardestGame {
       this.bouncingBall2.checkCollision(this.player);
       window.requestAnimationFrame(this.draw);
       this.player.win();
-      }
+    }
   }
 }
 function distance(a, b) {
   return Math.sqrt((a.xPos - b.xPos) ** 2 + (a.yPos - b.yPos) ** 2);
 }
+
 
 let hardestGame = new HardestGame(canvas.getContext("2d"));
