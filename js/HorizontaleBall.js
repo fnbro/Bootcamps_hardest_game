c9f44c897f109ecb7d9acdcc9a0b849279fff7bc
fnbro <54069595+fnbro@users.noreply.github.com>
2019-09-11 14:08:20 +0200

    Update HorizontaleBall.js

--- js/HorizontaleBall.js
+++ js/HorizontaleBall.js
@@ -1,16 +1,16 @@
 class HorizontaleBall {
-  constructor(context, xPos, yPos, deathCounter) {
+  constructor(context, xPos, yPos, dx, radius, deathCounter) {
     this.xPos = xPos;
     this.yPos = yPos;
-    this.dx = 35;
-    this.radius = 15;
+    this.dx = dx;
+    this.radius = radius;
     this.context = context;
     this.draw = this.draw.bind(this);
     this.checkCollision = this.checkCollision.bind(this);
     this.deathCounter = deathCounter;
   }
   draw() {
-    this.context.save() 
+    this.context.save()
     this.context.fillStyle = "black";
 
     this.context.beginPath();
@@ -19,6 +19,22 @@ class HorizontaleBall {
     this.context.stroke();
 
     if (this.xPos < 20 || this.xPos > 780) {
+      this.dx = -this.dx;
+    }
+    this.xPos += this.dx;
+    this.context.restore()
+
+  }
+  movingOfBall() {
+    this.context.save()
+    this.context.fillStyle = "black";
+
+    this.context.beginPath();
+    this.context.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI);
+    this.context.fill();
+    this.context.stroke();
+
+    if (this.xPos < 40 || this.xPos > 680) {
       this.dx = -this.dx;
     }
     this.xPos += this.dx;
